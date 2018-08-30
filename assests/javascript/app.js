d11fe6be6de6cd0f44caf27f10bb0cc6dfb835c3
YJ <yair.yoseph@gmail.com>
2018-08-29 18:21:13 -0700

    questionmaker function

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,45 +1,26 @@
-// var question = ["What kind of music do you like?", "Where would you rather be right now?", "What is your favorite pokemon?", "How do you feel about people who only drink sparkling"];
-// var correctAnswer = 0;
-// var incorrectAnswer = 0;
-// var unanswered = 0; 
+var correct = 0;
+var inccorect = 0;
+var unAnswered = 0;
+var quizOver = false;
 
-$("#button").on("click", check);
 
-function check() {
-    alert("You have one the grand prize!! One million dollars!!");
+var questions = [{
+    question: "Hi Michael?",
+    answers: ["Bye", "Good Morning!", "Good Night"],
+    correctAnswer: 2
+}, {
+    question: "Hi ?",
+    answers: ["Bye", "Good Morning!", "Good Night"],
+    correctAnswer: 2
+}];
 
-    var question1 = document.quiz.question1.value;
-    var question2 = document.quiz.question2.value;
-    var question3 = document.quiz.question3.value;
-    var question4 = document.quiz.question4.value;
-    var question5 = document.quiz.question5.value;
-    var question6 = document.quiz.question6.value;
-    var question7 = document.quiz.question7.value;
-    var correct = 0;
-
-    if (question1 == "answer") {
-        correct++;
+function questionMaker() {
+    for (let i = 0; i < questions.length; i++) {
+        var newRow = $("<div class='row'>");
+        var newColumn = $("<div class='col-md-12'>");
+        $("#mainContainer").append(newRow);
+        (newRow).append(newColumn);
+        (newColumn).html(questions[i].question);
     }
-    if (question2 == "answer") {
-        correct++;
-    }
-    if (question3 == "answer") {
-        correct++;
-    }
-    if (question4 == "answer") {
-        correct++;
-    }
-    if (question5 == "answer") {
-        correct++;
-    }
-    if (question6 == "answer") {
-        correct++;
-    }
-    if (question7 == "answer") {
-        correct++;
-    }
-
-document.getElementById("after_submit").style.visibility = "visible";
-document.getElementById("number_correct").innerHtml = "You got " + correct + " correct.";
-
 }
+questionMaker()
