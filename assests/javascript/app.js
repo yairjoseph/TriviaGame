1948c5727f05082ad958b9d880475bdaa5ccb47f
YJ <yair.yoseph@gmail.com>
2018-08-30 21:09:49 -0700

    almost there

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,30 +1,47 @@
 var correct = 0;
-var inccorect = 0;
+var incorrect = 0;
 var unAnswered = 0;
 var quizOver = false;
-var time = 5;
+var time = 61;
 var intervalId;
+var form = $("<form>");
 
 
 var questions = [{
-    question: "Hi Michael?",
-    answers: ["Bye ", "Good Morning! ", "Good Night "],
+    question: "Which year's top 10 grossing video games are available on PS4?",
+    answers: ["2011", "2015", "2014", "2010"],
+    correctAnswer: 1
+}, {
+    question: "Most of us have played ATARI games. what does 'Atari' mean?",
+    answers: ["Happiness", "Game", "Success", "Fun"],
     correctAnswer: 2
 }, {
-    question: "Hi ?",
-    answers: ["Shalom ", "Ola! ", "Night "],
-    correctAnswer: 3
+    question: "What does Mario jump onto after completing a level?",
+    answers: ["Tube", "Castle", "Flag Pole", "Stairs"],
+    correctAnswer: 2
 }, {
-    question: "Home!!",
-    answers: ["Today?", "Tomorrow?", "Now?", "How?"],
+    question: "What is another name for Counter-Strike?",
+    answers: ["Half-life", "CSGO", "1.6", "CS"],
+    correctAnswer: 0
+}, {
+    question: "Where was the first PS3 released?",
+    answers: ["Israel ", "United States ", "Japan", "China"],
+    correctAnswer: 2
+}, {
+    question: "Name the all time highest grossing game.",
+    answers: ["World of Warcraft", "Counter-Strike", "Call Of Duty", "League Of Legends"],
+    correctAnswer: 0
+}, {
+    question: "Name the character abused by Mario",
+    answers: ["Luigi", "Donkey Kong", "Peach", "Yoshi"],
     correctAnswer: 1
 }];
-
-function questionMaker() {
+///change ame for every question
+function QAMaker() {
     for (let i = 0; i < questions.length; i++) {
         var newRow = $("<div class='row headColor'>");
         var newColumn = $("<div class='col-md-12'>");
-        $(".container").append(newRow);
+        $("#mainContainer").append(newRow);
         (newRow).append(newColumn);
         (newColumn).html(questions[i].question);
 
@@ -32,41 +49,87 @@ function questionMaker() {
             var newRow = $("<div class='row'>");
             var newColumn = $("<div class='col-md-12 answerPreCss'>");
             var newAnswer = questions[i].answers[j];
-            var inputs = $("<input type='radio' name='answers'>");
+            var inputs = $("<input type='radio'>");
             var label = $("<label>");
             inputs.attr("id", "answers" + i + j);
             label.attr("for", "answers" + i + j);
-            label.text(questions[i].answers[j]);
-            $(".container").append(newRow);
+            label.text(newAnswer);
+            $("#mainContainer").append(newRow);
             (newRow).append(newColumn);
             (newColumn).append(inputs);
+            inputs.attr("name", "answers" + [i]);
+            inputs.attr("value", newAnswer);
             newColumn.append(label);
         }
     }
 }
-questionMaker()
+
+QAMaker()
 
 function count() {
     time--;
-    $(".timeRemaining").html("Time Remaning: " + time);
+    $("#timeLeft").html(time);
 
     if (time === 0) {
-        $(".container").hide();
+        $("#startContainer").hide();
+        $("#mainContainer").hide();
         $(".timeRemaining").hide();
+        $(".submit").hide();
+        $("#container3").show();
+        $("#triviaTime").hide();
         clearInterval(intervalId);
-    } console.log(time)
+    }
 }
+
 $(".start").on("click", runQuiz)
 
 function runQuiz() {
     intervalId = setInterval(count, 1000)
+    $(".start").hide();
+    $(".submit").show();
+    $("#startContainer").show();
+    $("#mainContainer").show();
+    $(".timeRemaining").show();
+    $("#triviaTime").show();
+    $("#triviaTime2").hide();
+
 }
 
+$(".submit").on("click", submit)
 
+function submit() {
+    $(".submit").hide();
+    $("#mainContainer").hide();
+    $(".timeRemaining").hide();
+    $("#container3").show();
+    $("#triviaTime").hide();
+    answersFunc();
+}
 
-// $(".container").show(); 
-// function endQuiz() {
-//     if (time === 0) {
+var rightAnswer = [
+    $("input[name='answers0']:checked").val(),
+    $("input[name='answers1']:checked").val(),
+    $("input[name='answers2']:checked").val(),
+    $("input[name='answers3']:checked").val(),
+    $("input[name='answers4']:checked").val(),
+    $("input[name='answers5']:checked").val(),
+    $("input[name='answers6']:checked").val(),
+]
 
-//     }
-// }
+function answersFunc() {
+    for (let i = 0; i < questions.length; i++) {
+
+        if (questions[i].answers.indexOf(rightAnswer[i]) === questions[i].correctAnswer) {
+            correct++;
+        }
+        else if (!rightAnswer[i]) {
+            unAnswered++;
+        }
+        else {
+            incorrect++;
+        }
+    }
+    $("#Correct").text(correct);
+    $("#Incorrect").text(incorrect);
+    $("#UnAnswered").text(unAnswered);
+}
